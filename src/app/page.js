a8dd6af652d12e11a06b4e4c5c2ffd54e0dd26d8
Daniel2231 <daniel2231.dev@gmail.com>
2024-03-19 15:02:10 +0900

    hotfix: fixed auth

--- src/app/page.js
+++ src/app/page.js
@@ -1,118 +1,124 @@
-"use client";
+'use client';
 
-import { useCallback, useState } from "react";
-import Head from "next/head";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useCallback, useState } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import {
-  Alert,
-  Box,
-  Button,
-  FormHelperText,
-  Link as MUILink,
-  Stack,
-  Tab,
-  Tabs,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { LoginLayout } from "@/layouts/loginLayout";
-import { useUserLoginApi } from "@/hooks/use-user";
-import { useSetRecoilState } from "recoil";
-import { accessToken } from "@/utils/recoil/atoms";
+	Alert,
+	Box,
+	Button,
+	FormHelperText,
+	Link as MUILink,
+	Stack,
+	Tab,
+	Tabs,
+	TextField,
+	Typography,
+} from '@mui/material';
+import { LoginLayout } from '@/layouts/loginLayout';
+import { useUserLoginApi } from '@/hooks/use-user';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { LoginState } from '@/utils/recoil/atoms';
 
 const Home = () => {
-  const router = useRouter();
-  const [id, setID] = useState("");
-  const [pw, setPW] = useState("");
-  const [login] = useUserLoginApi();
-  const setAccessToken = useSetRecoilState(accessToken);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    login(id, pw).then((token) => {
-      setAccessToken(token);
-      router.push("/account");
-    });
-  };
+	const router = useRouter();
+	const [id, setID] = useState('');
+	const [pw, setPW] = useState('');
+	const [login] = useUserLoginApi();
+	const [isLoggedIn, setIsLoggedIn] = useRecoilState(LoginState);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		login(id, pw).then((res) => {
+			if (res === true) {
+				setIsLoggedIn(true);
+				router.push('/account');
+			} else {
+				alert(`아이디 또는 비밀번호가 틀렸습니다. 다시 시도해주세요.
+        에러 메시지: ${res}
+        `);
+			}
+		});
+	};
 
-  return (
-    <LoginLayout>
-      <Head>
-        <title>SKKLUB 어드민</title>
-      </Head>
-      <Box
-        sx={{
-          backgroundColor: "background.paper",
-          flex: "1 1 auto",
-          alignItems: "center",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <Box
-          sx={{
-            maxWidth: 550,
-            px: 3,
-            py: "100px",
-            width: "100%",
-          }}
-        >
-          <div>
-            <Stack spacing={1} sx={{ mb: 3 }}>
-              <Typography variant="h4">SKKLUB ADMIN</Typography>
-              <Typography color="text.secondary" variant="body2">
-                <MUILink
-                  component={Link}
-                  href="/register"
-                  underline="hover"
-                  variant="subtitle2"
-                >
-                  회원가입하기
-                </MUILink>
-              </Typography>
-            </Stack>
-            <Tabs sx={{ mb: 3 }} value="ID">
-              <Tab label="Login" value="ID" />
-            </Tabs>
+	return (
+		<LoginLayout>
+			<Head>
+				<title>SKKLUB 어드민</title>
+			</Head>
+			<Box
+				sx={{
+					backgroundColor: 'background.paper',
+					flex: '1 1 auto',
+					alignItems: 'center',
+					display: 'flex',
+					justifyContent: 'center',
+				}}
+			>
+				<Box
+					sx={{
+						maxWidth: 550,
+						px: 3,
+						py: '100px',
+						width: '100%',
+					}}
+				>
+					<div>
+						<Stack spacing={1} sx={{ mb: 3 }}>
+							<Typography variant="h4">SKKLUB ADMIN</Typography>
+							<Typography color="text.secondary" variant="body2">
+								<MUILink
+									component={Link}
+									href="/register"
+									underline="hover"
+									variant="subtitle2"
+								>
+									회원가입하기
+								</MUILink>
+							</Typography>
+						</Stack>
+						<Tabs sx={{ mb: 3 }} value="ID">
+							<Tab label="Login" value="ID" />
+						</Tabs>
 
-            <form>
-              <Stack spacing={3}>
-                <TextField
-                  fullWidth
-                  label="ID"
-                  name="ID"
-                  onChange={(e) => setID(e.target.value)}
-                  type="ID"
-                  value={id}
-                  inputProps={{ maxLength: 14 }}
-                />
-                <TextField
-                  fullWidth
-                  label="Password"
-                  name="password"
-                  onChange={(e) => setPW(e.target.value)}
-                  type="password"
-                  value={pw}
-                  inputProps={{ maxLength: 14 }}
-                />
-              </Stack>
+						<form>
+							<Stack spacing={3}>
+								<TextField
+									fullWidth
+									label="ID"
+									name="ID"
+									onChange={(e) => setID(e.target.value)}
+									type="ID"
+									value={id}
+									inputProps={{ maxLength: 14 }}
+								/>
+								<TextField
+									fullWidth
+									label="Password"
+									name="password"
+									onChange={(e) => setPW(e.target.value)}
+									type="password"
+									value={pw}
+									inputProps={{ maxLength: 14 }}
+								/>
+							</Stack>
 
-              <Button
-                fullWidth
-                size="large"
-                sx={{ mt: 3 }}
-                type="submit"
-                variant="contained"
-                onClick={handleSubmit}
-              >
-                Continue
-              </Button>
-            </form>
-          </div>
-        </Box>
-      </Box>
-    </LoginLayout>
-  );
+							<Button
+								fullWidth
+								size="large"
+								sx={{ mt: 3 }}
+								type="submit"
+								variant="contained"
+								onClick={handleSubmit}
+							>
+								Continue
+							</Button>
+						</form>
+					</div>
+				</Box>
+			</Box>
+		</LoginLayout>
+	);
 };
 
 export default Home;
--- src/hooks/use-user.js
+++ src/hooks/use-user.js
@@ -1,58 +1,59 @@
-import axios from "axios";
-import { useState } from "react";
+import axios from 'axios';
+import { useState } from 'react';
 
 export const useUserLoginApi = () => {
-  const [accessToken, setAccess] = useState("");
-  const login = async (id, pw) => {
-    axios
-      .post(`/user/login?username=${id}&password=${pw}`)
-      .then((response) => {
-        localStorage.setItem("refresh", response.headers["refresh-token"]);
-        localStorage.setItem("userid", response.data.id);
-        localStorage.setItem("username", response.data.username);
-        localStorage.setItem("role", response.data.role);
-        setAccess(response.headers["authorization"]);
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("아이디나 비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
-      });
+	const [res, setRes] = useState(null);
+	const login = async (id, pw) => {
+		axios
+			.post(`/user/login?username=${id}&password=${pw}`)
+			.then((response) => {
+				localStorage.setItem(
+					'token',
+					response.headers.authorization.split(' ')[1]
+				);
+				localStorage.setItem('username', response.data.username);
+				localStorage.setItem('role', response.data.role);
+				setRes(true);
+			})
+			.catch((error) => {
+				console.log(error);
+				setRes(error);
+			});
+		return res;
+	};
 
-    return accessToken;
-  };
-
-  return [login];
+	return [login];
 };
 
 export const useUserLogoutApi = () => {
-  const logout = () => {
-    axios
-      .get(`/oauth2/kakao/logout`, {
-        withCredentials: true,
-      })
-      .then((response) => {
-        console.log(response);
-        // window.location.reload();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+	const logout = () => {
+		axios
+			.get(`/oauth2/kakao/logout`, {
+				withCredentials: true,
+			})
+			.then((response) => {
+				console.log(response);
+				// window.location.reload();
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
-  return [logout];
+	return [logout];
 };
 
 export const useUserEditApi = () => {
-  const editUser = ({ name, pw, contact }) => {
-    axios
-      .post(`/user/0?password=${pw}&name=${name}&contact=${contact}`)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+	const editUser = ({ name, pw, contact }) => {
+		axios
+			.post(`/user/0?password=${pw}&name=${name}&contact=${contact}`)
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
-  return [editUser];
+	return [editUser];
 };
